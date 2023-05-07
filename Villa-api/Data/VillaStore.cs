129ca9477821c6675dbb518f0abfd2a8096844e9
Daniel Ashorov <daniel_a@geoda.co.il>
2023-05-07 15:46:16 +0300

    Adding to versions of Loggers

---
+++ Villa-api/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using Villa_api.Models.Dto;
+
+namespace Villa_api.Data
+{
+    public class VillaStore
+    {
+        public static List <VillaDTO> villaList = new List<VillaDTO>
+            {
+                new VillaDTO {Id = 1, Name ="Pool View", Sqft = 100, Occupancy = 4},
+                new VillaDTO {Id = 2, Name = "Beach View", Sqft = 300, Occupancy = 3}
+            };
+    }
+}
--- Villa-api/Program.cs
+++ Villa-api/Program.cs
@@ -1,11 +1,21 @@
+using Serilog;
+using Villa_api.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Information().WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+//builder.Host.UseSerilog();
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(option =>
+{
+    //option.ReturnHttpNotAcceptable=true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<ILogging, LoggingV2>();
 
 var app = builder.Build();
 
